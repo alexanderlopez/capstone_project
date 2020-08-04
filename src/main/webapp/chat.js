9f283df2cbb7758d9a2e67a920b95f91422dba75
Alicia Stepin <aliciastepin@gmail.com>
2020-08-04 20:39:19 +0000

    intermediary changes: work on websocket frontend

--- src/main/webapp/chat.js
+++ src/main/webapp/chat.js
@@ -12,10 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://chap-2020-capstone.appspot.com/";
+  xhttp.onload = function() {
+      alert("ready state: "+ this.readyState + " status is: " + this.status);
+    if (this.readyState == 4 && this.status == 200) {
+        alert("aaaabbb");
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", url ,false);
+    xhttp.setRequestHeader('Cache-Control', 'no-cache');
+  alert("hello world");
+  xhttp.send(null);
+  alert("xxx");
+}
+
+
+//var connection = new WebSocket('https://chap-2020-capstone.appspot.com/');
+var connection = new WebSocket(getServerlUrl());
+
+connection.onopen = () => {
+  console.log('connected');
+};
+
+connection.onclose = () => {
+  console.error('disconnected');
+};
+
+connection.onerror = (error) => {
+  console.error('failed to connect', error);
+};
+
+connection.onmessage = (event) => {
+  console.log('received', event.data);
+  let li = document.createElement('li');
+  li.innerText = event.data;
+  document.querySelector('#chat').append(li);
+};
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  let message = document.querySelector('#message').value;
+  connection.send(message);
+  document.querySelector('#message').value = '';
+});
+
+
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+
+
+
+function getServerUrl() {
+    var defaultChatRoomID = "1234goroom";
+
+    if (location.protocol !== 'https:' && location.hostname != 'localhost:8080') {
+        location.replace(`https:${location.href.substring(location.protocol.length)}`);
+    }
+    return location.protocol + "://" + location.hostname + "/chat/" + defaultChatRoomID + "?idToken=" +tokenID;
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <%@ page import="com.google.sps.HelloAppEngine" %>
+
 <!-- [START_EXCLUDE] -->
+
 <!-- [END_EXCLUDE] -->
+
 <html>
     <head>
+       <script src="chat.js"></script>
         <script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
         <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
 
         <!-- The core Firebase JS SDK is always required and must be listed first -->
-        <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js"></script> 
 
         <!-- TODO: Add SDKs for Firebase products that you want to use
             https://firebase.google.com/docs/web/setup#available-libraries -->
+
         <script src="https://www.gstatic.com/firebasejs/7.17.1/firebase-auth.js"></script>
 
         <script src="authentication.js"></script>
 
         <link href='//fonts.googleapis.com/css?family=Marmelad' rel='stylesheet' type='text/css'>
         <title>Hello App Engine Flexible</title>
+
     </head>
-    <body>
-        <h1>Hello App Engine -- Flexible!</h1>
+    <body onload="loadDoc()">
+        
+
+        <h1>Hello App Engine -- Flexible!!!</h1>
 
         <p>This is <%= HelloAppEngine.getInfo() %>.</p>
         <table>
@@ -29,11 +39,14 @@
                 <td colspan="2" style="font-weight:bold;">Available Servlets:</td>
             </tr>
             <tr>
-                <td><a href='/hello'>The servlet</a></td>
+                <td><a href='chatroom.html'>The servlet</a></td>
             </tr>
         </table>
-
         <div id="firebaseui-auth-container"></div>
-        <div id="loader">Loading...</div>
+        <div id="loader">Loading...</div> 
+        <div id="demo"  onload="loadDoc()">X</div>
+         <div id="firebaseui-auth-container"></div> 
     </body>
 </html>
+
+
