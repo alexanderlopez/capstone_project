9d6815777a0d6b91d13fe8e2b7aa45debae00f80
Alexander Lopez <alexanderlopez00@gmail.com>
2020-08-13 21:10:40 -0400

    Fix dependency problems with integration testing by upgrading mockito version and powermockito.

--- src/test/java/com/google/sps/DatastoreManagerTest.java
+++ src/test/java/com/google/sps/DatastoreManagerTest.java
@@ -6,14 +6,23 @@ import java.util.concurrent.TimeoutException;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 
 import static org.mockito.Mockito.*;
+import org.mockito.BDDMockito;
+
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.api.mockito.PowerMockito;
 
 import com.google.cloud.datastore.testing.LocalDatastoreHelper;
 
 import com.google.sps.DatastoreManager;
+import com.google.sps.CapstoneAuth;
 
+@PrepareForTest(CapstoneAuth.class)
+@RunWith(PowerMockRunner.class)
 public class DatastoreManagerTest {
 
     LocalDatastoreHelper datastoreHelper =
@@ -39,6 +48,10 @@ public class DatastoreManagerTest {
         String uid = "testuid";
         String name = "Test User";
         String email = "test@example.com";
+
+        PowerMockito.mockStatic(CapstoneAuth.class);
+        BDDMockito.given(CapstoneAuth.getUserEmail(uid)).willReturn(email);
+        //PowerMockito.verifyStatic();
 
         DatastoreManager.getInstance().createUser(uid, name);
 
